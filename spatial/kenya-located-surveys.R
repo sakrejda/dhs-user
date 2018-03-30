22bd82b77f50b58530cf362e9ba47f3d22aca703
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2018-03-29 22:22:00 -0400

    Wut?

--- spatial/kenya-located-surveys.R
+++ spatial/kenya-located-surveys.R
@@ -5,7 +5,7 @@ located_clusters <- readRDS('kenya-located-clusters.rds')
 located_surveys <- dplyr::left_join(
   x = surveys, 
   y = located_clusters %>% dplyr::select(cluster_id, year, county_name, 
-    area, perimeter, longitude, latitude, stratum, county_idx, weight)
+    area, perimeter, longitude, latitude, stratum, county_idx)
 )
 
 
