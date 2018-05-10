cd7d23e7ec36437f3ae7d31bf321d31529df8add
Krzysztof Sakrejda <sakrejda@umass.edu>
2018-05-10 15:18:02 -0400

    Minor.

--- spatial/radial-splines.R
+++ spatial/radial-splines.R
@@ -22,9 +22,36 @@ basis = function(x, p, b, h) {
   for (i in 1:length(p)) {
     o[,i] = basis_function(abs(x - p[i]), h[i])
   }
-  o = apply(o, 1, function(x) sum( (b/sum(x)) * x)/sum(x))
+  o = apply(o, 1, function(x) sum(b * x))
   return(o)
 }
+
+## 2d check
+grid <- expand.grid(x = seq(-40, 40, 1), y=seq(-40, 40, 1), U=0)
+line <- seq(from = -40, to = 40, by = 0.01)
+line_U <- vector(mode = 'numeric', length = length(line))
+h = 0.95
+for ( i in 1:nrow(grid)) for( j in 1:length(line)) {
+  r = sqrt((grid[['x']][i] - line[j])^2 + (grid[['y']][i] - line[j])^2)
+  if (r > 2 * h) 
+    next
+  line_U[j] = line_U[j] + basis_function(r, h)
+}
+
+lines(line, line_U, type = 'l')
+
+
+line <- seq(from = -40, to = 40, by = 0.01)
+line_U <- vector(mode = 'numeric', length = length(line))
+h = 0.95
+for ( i in 1:nrow(grid)) for( j in 1:length(line)) {
+  r = sqrt((grid[['x']][i] - line[j])^2 + (grid[['y']][i] - 0)^2)
+  if (r > 2 * h) 
+    next
+  line_U[j] = line_U[j] + basis_function(r, h)
+}
+
+plot(line, line_U, type = 'l')
 
 
 
